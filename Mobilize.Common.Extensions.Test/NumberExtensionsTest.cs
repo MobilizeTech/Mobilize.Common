9851b9a843e585a5d2252fa1cf29f36401e303cb
Kevin Bowling <kevin@mobilize.tech>
2016-10-01 13:42:55 -1000

    first round of extensions

---
+++ Mobilize.Common.Extensions.Test/NumberExtensionsTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Mobilize.Common.Extensions.Test
+{
+    [TestClass]
+    public class NumberExtensionsTest
+    {
+        [TestMethod]
+        public void TestIsPrime()
+        {
+            int[] primes = new int[] { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109 };
+            int[] nonPrimes = new int[] { 4, 6, 8, 9, 10, 12, 14, 15, 16, 18, 20, 21, 22, 24, 25, 26, 27, 28, 30, 32, 33, 34, 35, 36, 38, 39, 40, 42, 44, 45, 46, 48 };
+
+            foreach (int prime in primes)
+            {
+                Assert.IsTrue(prime.IsPrime());
+            }
+
+            foreach (int nonPrime in nonPrimes)
+            {
+                Assert.IsFalse(nonPrime.IsPrime());
+            }
+        }
+
+        [TestMethod]
+        public void TestToFileSize()
+        {
+            long oneMegabyte = 1048576;
+            Assert.AreEqual("1MB", oneMegabyte.ToFileSize());
+
+            long fortyGigabytes = 42949672960;
+            Assert.AreEqual("40GB", fortyGigabytes.ToFileSize());
+        }
+    }
+}
---
+++ Mobilize.Common.Extensions/NumberExtensions.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Linq;
+
+namespace Mobilize.Common.Extensions
+{
+    public static class NumberExtensions
+    {
+        /// <summary>
+        /// Formats a number into bytes
+        /// </summary>
+        /// <param name="l">Number to format</param>
+        /// <returns>String representing size in bytes</returns>
+        public static string ToFileSize(this long l)
+        {
+            if (l < 1024)
+            {
+                return (l).ToString("F0") + " bytes";
+            }
+
+            if (l < Math.Pow(1024, 2))
+            {
+                return (l / 1024).ToString("F0") + "KB";
+            }
+
+            if (l < Math.Pow(1024, 3))
+            {
+                return (l / Math.Pow(1024, 2)).ToString("F0") + "MB";
+            }
+
+            if (l < Math.Pow(1024, 4))
+            {
+                return (l / Math.Pow(1024, 3)).ToString("F0") + "GB";
+            }
+
+            if (l < Math.Pow(1024, 5))
+            {
+                return (l / Math.Pow(1024, 4)).ToString("F0") + "TB";
+            }
+
+            if (l < Math.Pow(1024, 6))
+            {
+                return (l / Math.Pow(1024, 5)).ToString("F0") + "PB";
+            }
+
+            return (l / Math.Pow(1024, 6)).ToString("F0") + "EB";
+        }
+
+        /// <summary>
+        /// Checks if an integer is prime
+        /// </summary>
+        /// <param name="n">The integer to check</param>
+        /// <returns>Boolean</returns>
+        public static bool IsPrime(this int n)
+        {
+            if (n < 3) return (n == 2);
+            return Enumerable.Range(2, (int)Math.Sqrt(n)).All(m => n % m != 0);
+        }
+    }
+}
