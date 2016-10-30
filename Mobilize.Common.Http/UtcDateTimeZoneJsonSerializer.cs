6832dbb37dd76486f17a3f72e2ed488bffca1cd1
Kelly Murphy <kelly@mobilize.tech>
2016-10-29 18:07:47 -1000

    refactored deserialize + added ability to pass json serializer settings

---
+++ Mobilize.Common.Http/UtcDateTimeZoneJsonSerializer.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+
+namespace Mobilize.Common.Http
+{
+    /// <summary>
+    /// Deserializes JSON with UTC date/times
+    /// </summary>
+    public static class UtcDateTimeZoneJsonSerializer
+    {
+        public static readonly JsonSerializerSettings Settings = new JsonSerializerSettings
+        {
+            DateTimeZoneHandling = DateTimeZoneHandling.Utc
+        };
+
+        public static string SerializeObject(object o)
+        {
+            return JsonConvert.SerializeObject(o, Formatting.Indented, Settings);
+        }
+
+        public static T DeserializeObject<T>(string s)
+        {
+            return JsonConvert.DeserializeObject<T>(s, Settings);
+        }
+    }
+}
